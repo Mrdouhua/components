76b61632b1588ee366d3a44fe510eeb5d5d76bf3
Mrdouhua <1377392896@qq.com>
2017-02-11 10:47:56 +0800

    添加事件

--- tab/tab.js
+++ tab/tab.js
@@ -1,8 +1,12 @@
 window.onload = function(){
-	tab("tab_menu","items","select");
+	tab("tab_menu","items","select","click");
 }
-// 编写可复用的tab选项卡
-function tab(id,items,select){
+
+/*
+* tab函数接受四个参数
+* 标题ul的id，内容部分盒子的class，标题高亮显示的class，添加的事件
+*/
+function tab(id,items,select,ev){
 	var oUl = document.getElementById(id);
 	var oLi = oUl.getElementsByTagName('li');
 	var items = document.getElementsByClassName(items);
@@ -10,7 +14,7 @@ function tab(id,items,select){
 	for(var i = 0, len = oLi.length; i < len; i++) {
 		oLi[i].index = i;
 		items[i].index = i;
-		oLi[i].addEventListener("click", function(){
+		oLi[i].addEventListener(ev, function(){
 			for(var j = 0, Len = oLi.length; j < Len; j++) {
 				oLi[j].className = '';
 				items[j].style.display = "none";
